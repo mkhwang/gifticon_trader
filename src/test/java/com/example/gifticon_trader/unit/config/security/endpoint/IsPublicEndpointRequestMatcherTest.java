cffc7d1537159684019b5bcd88ec5ce4ccde2112
mkhwnag <hmk6264@gmail.com>
2025-04-17 22:48:51 +0900

    add public end point matcher test

---
+++ src/test/java/com/example/gifticon_trader/unit/config/security/endpoint/IsPublicEndpointRequestMatcherTest.java
@@ -0,0 +1,45 @@
+package com.example.gifticon_trader.unit.config.security.endpoint;
+
+import com.example.gifticon_trader.config.security.endpoint.IsPublicEndpoint;
+import com.example.gifticon_trader.config.security.endpoint.IsPublicEndpointRequestMatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.HandlerMapping;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class IsPublicEndpointRequestMatcherTest {
+  @Test
+  void shouldReturnTrue_whenHandlerHasAnnotation() throws Exception {
+    // given
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+    HandlerMethod handlerMethod = new HandlerMethod(
+            new AnnotatedController(), AnnotatedController.class.getMethod("publicEndPoint")
+    );
+
+    HandlerExecutionChain chain = new HandlerExecutionChain(handlerMethod);
+
+    HandlerMapping mapping = mock(HandlerMapping.class);
+    when(mapping.getHandler(mockRequest)).thenReturn(chain);
+
+    IsPublicEndpointRequestMatcher matcher = new IsPublicEndpointRequestMatcher(List.of(mapping));
+
+    // when
+    boolean result = matcher.matches(mockRequest);
+
+    // then
+    assertThat(result).isTrue();
+  }
+
+  static class AnnotatedController {
+    @IsPublicEndpoint
+    public void publicEndPoint() {
+    }
+  }
+}
